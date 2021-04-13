e340debad2fd0f63f00efd003bc830fc16ed30a2
zhangyilin <niliyzhang@foxmail.com>
2021-04-13 11:37:24 +0800

    路由配置

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import React from 'react';
+import { renderRoutes } from 'react-router-config';//renderRoutes 读取路由配置转化为 Route 标签
+import { HashRouter } from 'react-router-dom';
+import routes from './routes/index.js';
+
 import {GlobalStyle} from './style';
 import { IconStyle } from './assets/iconfont/iconfont';
 
 function App() {
   return (
-    <div className="App">
+    <HashRouter>
       <GlobalStyle></GlobalStyle>
       <IconStyle></IconStyle>
-      <span class="iconfont">&#xe8ac;</span>
-    </div>
+      {renderRoutes(routes)}
+    </HashRouter>
   );
 }
 
