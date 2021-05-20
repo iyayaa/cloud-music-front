d04975b3c3947eaac8e55980b1fb4e7089fcdd23
zhangyilin <niliyzhang@foxmail.com>
2021-05-20 18:08:17 +0800

    歌手列表布局

--- src/baseUI/horizen-item/index.js
+++ src/baseUI/horizen-item/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, memo } from 'react';
+import React, { useRef, useEffect, memo } from 'react';
 import { PropTypes } from 'prop-types';
 import styled from'styled-components';
 import style from '../../assets/global-style';
