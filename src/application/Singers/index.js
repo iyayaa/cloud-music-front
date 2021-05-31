61f493516cc1dc660f099de9f2efb82bf2c857ed
zhangyilin <niliyzhang@foxmail.com>
2021-05-31 14:09:55 +0800

    歌手列表数据请求

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,8 +1,20 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import Horizen from '../../baseUI/horizen-item';
 import Scroll from '../../baseUI/scroll';
 import { categoryTypes,alphaTypes } from '../../api/config';
 import { NavContainer,ListContainer,List,ListItem } from "./style";
+
+import { 
+  getSingerList, 
+  getHotSingerList, 
+  changeEnterLoading, 
+  changePageCount, 
+  refreshMoreSingerList, 
+  changePullUpLoading, 
+  changePullDownLoading, 
+  refreshMoreHotSingerList 
+} from './store/actionCreators';
+import {connect} from 'react-redux';
 
 function Singers(props) {
   let [category, setCategory] = useState('');
@@ -10,26 +22,36 @@ function Singers(props) {
 
   let handleUpdateAlpha = (val) => {
     setAlpha(val);
+    updateDispatch(val, alpha);
   }
 
   let handleUpdateCatetory = (val) => {
     setCategory(val);
+    updateDispatch(category, val);
+    
   }
 
-  //mock 歌手数据
-  const singerList = [1, 2,3, 4,5,6,7,8,9,10,11,12].map (item => {
-    return {
-      picUrl: "https://p2.music.126.net/uTwOm8AEFFX_BYHvfvFcmQ==/109951164232057952.jpg",
-      name: "隔壁老樊",
-      accountId: 277313426,
-    }
-  }); 
+  const { singerList,
+    //  enterLoading, pullUpLoading, pullDownLoading, pageCount 
+    } = props;
+  const { getHotSingerDispatch, updateDispatch,
+    //  pullDownRefreshDispatch, pullUpRefreshDispatch 
+    } = props;
+
+  useEffect(() => {
+    getHotSingerDispatch();
+    // eslint-disable-next-line
+  }, []);
+
   // 渲染函数，返回歌手列表
   const renderSingerList = () => {
+
+    const list = singerList ? singerList.toJS(): [];
+
     return (
       <List>
         {
-          singerList.map((item, index) => {
+          list.map((item, index) => {
             return (
               <ListItem key={item.accountId+""+index}>
                 <div className="img_wrapper">
@@ -60,4 +82,44 @@ function Singers(props) {
   )
 }
 
-export default React.memo(Singers);
+const mapStateToProps = (state) => ({
+  singerList: state.getIn(['singers', 'singerList']),
+  enterLoading: state.getIn(['singers', 'enterLoading']),
+  pullUpLoading: state.getIn(['singers', 'pullUpLoading']),
+  pullDownLoading: state.getIn(['singers', 'pullDownLoading']),
+  pageCount: state.getIn(['singers', 'pageCount'])
+});
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getHotSingerDispatch() {
+      dispatch(getHotSingerList());
+    },
+    updateDispatch(category, alpha) {
+      dispatch(changePageCount(0));//由于改变了分类，所以pageCount清零
+      dispatch(changeEnterLoading(true));
+      dispatch(getSingerList(category, alpha));
+    },
+    // 滑到最底部刷新部分的处理
+    // pullUpRefreshDispatch(category, alpha, hot, count) {
+    //   dispatch(changePullUpLoading(true));
+    //   dispatch(changePageCount(count+1));
+    //   if(hot){
+    //     dispatch(refreshMoreHotSingerList());
+    //   } else {
+    //     dispatch(refreshMoreSingerList(category, alpha));
+    //   }
+    // },
+    // //顶部下拉刷新
+    // pullDownRefreshDispatch(category, alpha) {
+    //   dispatch(changePullDownLoading(true));
+    //   dispatch(changePageCount(0));//属于重新获取数据
+    //   if(category === '' && alpha === ''){
+    //     dispatch(getHotSingerList());
+    //   } else {
+    //     dispatch(getSingerList(category, alpha));
+    //   }
+    // }
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Singers));
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux-immutable'   //项目中需要用到 immutable.js 中的数据结构，所以合并不同模块 reducer 的时候需要用到 redux-immutable 中的方法
 import { reducer as recommendReducer } from '../application/Recommend/store/index';
+import { reducer as singersReducer } from '../application/Singers/store/index';
 
 export default combineReducers({
-  // 之后开发具体功能模块的时候添加 reducer
   recommend: recommendReducer,
+  singers: singersReducer,
 })
