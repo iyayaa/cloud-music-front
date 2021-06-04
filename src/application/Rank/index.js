dbaafb97b93a45c4121df6f372e7ceed6d8e30cb
zhangyilin <niliyzhang@foxmail.com>
2021-06-04 14:04:35 +0800

    排行榜 redux 开发

--- src/application/Rank/index.js
+++ src/application/Rank/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';//connect()根据配置信息，返回一个注入了 state 和 action creator 的 React 组件
+import { getRankList } from './store'
 
 function Rank (props) {
   return (
@@ -6,4 +8,18 @@ function Rank (props) {
   )
 }
 
-export default React.memo(Rank);
+// 映射 Redux 全局的 state 到组件的 props 上
+const mapStateToProps = (state) => ({
+  rankList: state.getIn(['rank', 'rankList']),
+  loading: state.getIn(['rank', 'loading']),
+});
+// 映射 dispatch 到 props 上
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getRankListDataDispatch() {
+      dispatch(getRankList());
+    }
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Rank));
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux-immutable'   //项目中需要用到 immutable.js 中的数据结构，所以合并不同模块 reducer 的时候需要用到 redux-immutable 中的方法
 import { reducer as recommendReducer } from '../application/Recommend/store/index';
 import { reducer as singersReducer } from '../application/Singers/store/index';
+import { reducer as rankReducer } from '../application/Rank/store/index';
 
 export default combineReducers({
   recommend: recommendReducer,
   singers: singersReducer,
+  rank: rankReducer
 })
