71fede7e78134aaa1902c497482cf345d758c3ef
zhangyilin <niliyzhang@foxmail.com>
2021-06-10 15:25:11 +0800

    播放组件布局

--- src/application/Player/index.js
+++ src/application/Player/index.js
@@ -10,9 +10,22 @@ import {
   changeFullScreen
 } from "./store/actionCreators";
 
+import MiniPlayer from './miniPlayer';
+import NormalPlayer from './normalPlayer';
+
 function Player(props) {
+
+  const currentSong = {
+    al: { picUrl: "https://p1.music.126.net/JL_id1CFwNJpzgrXwemh4Q==/109951164172892390.jpg" },
+    name: "木偶人",
+    ar: [{name: "薛之谦"}]
+  }
+
   return (
-    <div>Player</div>
+    <div>
+      <NormalPlayer song={currentSong}></NormalPlayer>
+      <MiniPlayer song={currentSong}/>
+    </div>
   )
 }
 
