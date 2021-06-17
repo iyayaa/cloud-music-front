7a13b13869353b5659c9ff8217c7b74717ef591c
zhangyilin <niliyzhang@foxmail.com>
2021-06-17 10:27:01 +0800

    进度条互交

--- src/baseUI/progress-bar/index.js
+++ src/baseUI/progress-bar/index.js
@@ -37,11 +37,62 @@ const ProgressBarWrapper = styled.div`
 `
 
 function ProgressBar(props) {
+
+  const progressBar = useRef();
+  const progress = useRef();
+  const progressBtn = useRef();
+  const [touch, setTouch] = useState({});
+
+  const progressBtnWidth = 16; 
+
+  // 处理进度条的偏移
+  const _offset = (offsetWidth) => {
+    progress.current.style.width = `${offsetWidth}px`;
+    progressBtn.current.style.transform = `translate3d(${offsetWidth}px, 0, 0)`;
+  }
+
+  //手动拖动
+
+  const progressTouchStart = (e) => {
+    // console.log(e)
+    // debugger
+    const startTouch = {};
+    startTouch.initiated = true;//initial 为 true 表示滑动动作开始了
+    startTouch.startX = e.touches[0].pageX;// 滑动开始时横向坐标
+    startTouch.left = progress.current.clientWidth;// 当前 progress 长度
+    setTouch(startTouch);
+  }
+
+  const progressTouchMove = (e) => {
+    if (!touch.initiated) return;
+    // 滑动距离   
+    const deltaX = e.touches[0].pageX - touch.startX;
+    const barWidth = progressBar.current.clientWidth - progressBtnWidth; 
+    const offsetWidth = Math.min(Math.max(0, touch.left + deltaX), barWidth);
+    _offset(offsetWidth);
+  }
+
+  const progressTouchEnd = (e) => {
+    const endTouch = JSON.parse(JSON.stringify(touch));
+    endTouch.initiated = false;
+    setTouch(endTouch);
+  }
+  //点击进度条时
+  const progressClick = (e) => {
+    const rect = progressBar.current.getBoundingClientRect();//Element.getBoundingClientRect() 方法返回元素的大小及其相对于视口的位置。
+    const offsetWidth = e.pageX - rect.left;
+    _offset(offsetWidth);
+  };
+
   return (
     <ProgressBarWrapper>
-      <div className="bar-inner">
-        <div className="progress"></div>
-        <div className="progress-btn-wrapper">
+      <div className="bar-inner" ref={progressBar} onClick={progressClick}>
+        <div className="progress" ref={progress}></div>
+        <div className="progress-btn-wrapper" ref={progressBtn}
+          onTouchStart={progressTouchStart}
+          onTouchMove={progressTouchMove}
+          onTouchEnd={progressTouchEnd}
+        >
           <div className="progress-btn"></div>
         </div>
       </div>
