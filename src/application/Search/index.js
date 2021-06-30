91927fc75cfe0763d1a6dca6a5611d7261e043ca
zhangyilin <niliyzhang@foxmail.com>
2021-06-30 16:00:46 +0800

    搜索模块redux开发

--- src/application/Search/index.js
+++ src/application/Search/index.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect,useCallback} from 'react';
 import { CSSTransition } from 'react-transition-group';
 import { Container } from './style';
 import SearchBox from './../../baseUI/search-box/index';
+import { connect } from 'react-redux';
+import { getHotKeyWords, changeEnterLoading, getSuggestList } from './store/actionCreators';
 
 function Search(props){
   // 控制动画
@@ -39,4 +41,28 @@ function Search(props){
   )
 }
 
-export default Search;
+// 映射 Redux 全局的 state 到组件的 props 上
+const mapStateToProps = (state) => ({
+  hotList: state.getIn(['search', 'hotList']),
+  enterLoading: state.getIn(['search', 'enterLoading']),
+  suggestList: state.getIn(['search', 'suggestList']),
+  songsCount: state.getIn(['player', 'playList']).size,
+  songsList: state.getIn(['search', 'songsList'])
+});
+
+// 映射 dispatch 到 props 上
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getHotKeyWordsDispatch() {
+      dispatch(getHotKeyWords());
+    },
+    changeEnterLoadingDispatch(data) {
+      dispatch(changeEnterLoading(data))
+    },
+    getSuggestListDispatch (data) {
+      dispatch(getSuggestList(data));
+    },
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Search));
